f1d17a710542601f82967a305e3723f561aba1f3
Muhammad Subhan <sbhnamir456@gmail.com>
2026-08-04 19:57:16 +0500

    Dockerized the application

--- week3_4th_august_2026_Thursday/app/schemas.py
+++ week3_4th_august_2026_Thursday/app/schemas.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from pydantic import BaseModel, ConfigDict, EmailStr
 
 
-
 class UserRegister(BaseModel):
     username: str
     email: EmailStr
@@ -25,6 +24,15 @@ class TokenData(BaseModel):
     role: str
 
 
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    role: str
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
 
 class CategoryCreate(BaseModel):
     name: str
@@ -35,7 +43,6 @@ class CategoryResponse(BaseModel):
     name: str
 
     model_config = ConfigDict(from_attributes=True)
-
 
 
 class NoteCreate(BaseModel):
@@ -58,15 +65,5 @@ class NoteResponse(BaseModel):
 
     owner: UserResponse
     category: CategoryResponse | None = None
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class UserResponse(BaseModel):
-    id: int
-    username: str
-    email: EmailStr
-    role: str
-    created_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
